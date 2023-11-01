e55835d6cd4384dc5b3e438052d2e1cf57f6287b
AYOUBZAKI1 <ayoubbenzaki2209@gmail.com>
2023-11-02 00:41:22 +0100

    correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 17:43:34 2020
+
+@author: Robinson Montes
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two integers.
+    """
+    adds two integers
 
     Args:
-        a: the first integer.
-        b: the second integer, default 98.
+        a (int): First integer to add
+        b (int): Second integer to add
 
     Raises:
-        TypeError: if a, b are not int, float.
+        TypeError: Exception if size is not an integer
 
-    Returns:
-        The sum of the two integers.
     """
-
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
+    if type(a) is not int:
+        if type(a) is float and a == a and abs(a) <= 1.7976931348623158e+308:
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if type(b) is not int:
+        if type(b) is float and b == b and abs(b) <= 1.7976931348623158e+308:
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
