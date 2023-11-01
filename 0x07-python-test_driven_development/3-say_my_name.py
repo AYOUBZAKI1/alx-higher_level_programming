f24c421da994ff17bcffdf129496e8b813dbdfa2
AYOUBZAKI1 <ayoubbenzaki2209@gmail.com>
2023-11-02 00:50:38 +0100

    correction

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
-Defines a function that prints my name
+Created on Thu May 21 17:43:34 2020
+
+@author: Robinson Montes
 """
 
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints "My name is [full name]"
+    priint a full name
+
+    Args:
+        first_name (str): First string to print
+        last_name (str): second string to print
+
+    Raises:
+        TypeError: Exception if arguments aren't string
     """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    elif type(last_name) is not str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    else:
-        print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
